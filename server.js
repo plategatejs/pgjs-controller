eaaaca19013d544eb3c6ce2c404ae88cea261cef
kazet15 <kazet15@gmail.com>
2015-06-06 12:40:13 +0200

    add image grabber and amqp configuration

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-var config = require('config');
-var amqp = require('amqp');
+var bootstrap = require('./lib/bootstrap.js');
+
+bootstrap
+    .then(function (data) {
+        var grabber = data.grabber;
+        var amqpConnection = data.amqpConnection;
+
+        // TODO putting received images on queue
+
+        grabber.on('image', function (image) {
+            console.log('received image');
+        });
+
+        grabber.on('error', function (error) {
+            console.log('error: ' + error);
+        });
+
+        grabber.start();
+    })
+    .catch(function (e) {
+        console.log(e);
+    });
