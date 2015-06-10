6952fcc1ae8fc88b1e0e611eb2ae029b6e003116
kazet15 <kazet15@gmail.com>
2015-06-10 15:15:09 +0200

    add mongodb connection for plate checking

--- server.js
+++ server.js
@@ -16,19 +16,43 @@ var host = config.get('amqp.host'),
 var Queue = require('./lib/queue.js'),
     queue = new Queue(host, port, username, password);
 
-queue.onPlates(function (plates) {
-    if (plates.hasOwnProperty('results') &&
-        Object.prototype.toString.call(plates.results) === '[object Array]'
-    ) {
-        var i = 1;
-        plates.results.forEach(function (result) {
-            console.log('Plate ' + i++);
+// setting database up
+var database = require('./lib/database.js');
 
-            result.candidates.forEach(function (candidate) {
-                console.log('Plate: ' + candidate.plate + ', confidence: ' + candidate.confidence + '.');
+// plate recognition logic
+var onPlatesRecognized = function (mongoose) {
+    return function (plates) {
+        var results = plates.results || [];
+        results = Array.isArray(results) ? results : [];
+
+        var platesToCheck = [];
+
+        results.forEach(function (result) {
+            var candidates = result.candidates || [];
+            candidates = Array.isArray(candidates) ? candidates.slice(0, 10) : [];
+
+            candidates.forEach(function (candidate) {
+                platesToCheck.push(candidate.plate);
             });
         });
-    }
+
+        var Plate = mongoose.model('plate');
+
+        // checking whether at least one of the recognized plates is allowed to enter
+        Plate.find({
+            value: {
+                '$in': platesToCheck
+            }
+        }, function (err, plates) {
+            if (!err && !!plates.length) {
+                console.log('Opening the gate.');
+            }
+        });
+    };
+};
+
+database(function (mongoose) {
+    queue.onPlates(onPlatesRecognized(mongoose));
 });
 
 queue.connect(function () {
